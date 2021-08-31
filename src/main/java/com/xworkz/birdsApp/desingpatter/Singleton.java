e816abd4d6f46b68331926f771e1a2bd2247d7b0
muheeth <38394176+muheethrahman@users.noreply.github.com>
2021-08-31 08:05:44 +0530

    Update Singleton.java

--- src/main/java/com/xworkz/birdsApp/desingpatter/Singleton.java
+++ src/main/java/com/xworkz/birdsApp/desingpatter/Singleton.java
@@ -8,14 +8,14 @@ public class Singleton {
 	private static SessionFactory factory;
 	public static SessionFactory getFactory() {
 		if(factory !=null && factory.isClosed()) {
-			throw new IllegalStateException("some one closed SessionFactory,please cprrect the code");
+			throw new IllegalStateException("closed SessionFactory");
 		}
 		return factory;
 	}
 
 	static {
 		try {
-			System.out.println("SF Started..");
+			System.out.println(" Started..");
 			Configuration conf=new Configuration().configure();
 			factory=conf.buildSessionFactory();
 			System.out.println("Sf complete..");
